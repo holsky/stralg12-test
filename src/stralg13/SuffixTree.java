2d0e1bbc8acd0f8f939054fbdf954e41cfa2724d
holger-s <holger.s@fu-berlin.de>
2013-04-23 21:48:04 +0200

    - initial commit

---
+++ src/stralg13/SuffixTree.java
@@ -0,0 +1,14 @@
+package stralg13;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SuffixTree {
+
+	@Test
+	public void testSimple() {
+		SuffixTree tree = new SuffixTree();
+		assertNotNull(tree);
+	}
+}
