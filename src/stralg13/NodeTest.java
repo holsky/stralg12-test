2de8ac4cc745ecf6c6399ea3a8344bf12f9f2068
holger-s <holger.s@fu-berlin.de>
2013-04-24 22:00:32 +0200

    - added splitEdge

--- src/stralg13/NodeTest.java
+++ src/stralg13/NodeTest.java
@@ -9,28 +9,28 @@ public class NodeTest {
 	@Test
 	public void nodeEqualsSimple() {
 		Node root1 = new Node();
-		root1.addEdge(0, 2);
-		root1.addEdge(1, 2);
+		root1.addEdgeAndNewNode(0, 2);
+		root1.addEdgeAndNewNode(1, 2);
 		Node root2 = new Node();
-		root2.addEdge(0, 2);
-		root2.addEdge(1, 2);
+		root2.addEdgeAndNewNode(0, 2);
+		root2.addEdgeAndNewNode(1, 2);
 		assertTrue(root1.equals(root2));
 	}
 	@Test
 	public void nodeEqualsDeepTree()
 	{
 	    Node root1 = new Node();
-	    root1.addEdge(0, 1);
-	    root1.addEdge(1, 2);
+	    root1.addEdgeAndNewNode(0, 1);
+	    root1.addEdgeAndNewNode(1, 2);
 
-	    root1.edges.get(new Tuple(0,1)).addEdge(2,3);
-	    root1.edges.get(new Tuple(1,2)).addEdge(3,4);
+	    root1.edges.get(new Tuple(0,1)).addEdgeAndNewNode(2,3);
+	    root1.edges.get(new Tuple(1,2)).addEdgeAndNewNode(3,4);
 
 	    Node root2 = new Node();
-	    root2.addEdge(0, 1);
-	    root2.addEdge(1, 2);
-	    root2.edges.get(new Tuple(0,1)).addEdge(2,3);
-	    root2.edges.get(new Tuple(1,2)).addEdge(3,4);
+	    root2.addEdgeAndNewNode(0, 1);
+	    root2.addEdgeAndNewNode(1, 2);
+	    root2.edges.get(new Tuple(0,1)).addEdgeAndNewNode(2,3);
+	    root2.edges.get(new Tuple(1,2)).addEdgeAndNewNode(3,4);
 
 	    assertTrue(root1.equals(root2));
 	}
@@ -38,18 +38,32 @@ public class NodeTest {
 	public void nodeNotEqualsDeepTree()
 	{
 	    Node root1 = new Node();
-	    root1.addEdge(0, 1);
-	    root1.addEdge(1, 2);
-	    root1.edges.get(new Tuple(0,1)).addEdge(2,3);
-	    root1.edges.get(new Tuple(1,2)).addEdge(3,4);
-	    root1.edges.get(new Tuple(0,1)).edges.get(new Tuple(2,3)).addEdge(4,5);
+	    root1.addEdgeAndNewNode(0, 1);
+	    root1.addEdgeAndNewNode(1, 2);
+	    root1.edges.get(new Tuple(0,1)).addEdgeAndNewNode(2,3);
+	    root1.edges.get(new Tuple(1,2)).addEdgeAndNewNode(3,4);
+	    root1.edges.get(new Tuple(0,1)).edges.get(new Tuple(2,3)).addEdgeAndNewNode(4,5);
 	    
 	    Node root2 = new Node();
-	    root2.addEdge(0, 1);
-	    root2.addEdge(1, 2);
-	    root2.edges.get(new Tuple(0,1)).addEdge(2,3);
-	    root2.edges.get(new Tuple(1,2)).addEdge(3,4);
+	    root2.addEdgeAndNewNode(0, 1);
+	    root2.addEdgeAndNewNode(1, 2);
+	    root2.edges.get(new Tuple(0,1)).addEdgeAndNewNode(2,3);
+	    root2.edges.get(new Tuple(1,2)).addEdgeAndNewNode(3,4);
 
 	    assertTrue(!root1.equals(root2));
+	}
+	
+	@Test
+	public void splitEdge() {
+		Node actual = new Node();
+		actual.addEdgeAndNewNode(0, 4);
+		actual.splitEdge(new Tuple(0,4), 2);
+		
+		Node expected = new Node();
+		Node child = new Node(expected);
+		expected.edges.put(new Tuple(0,2), child);
+		child.edges.put(new Tuple(2,4), new Node(child));
+		
+		assertTrue(expected.equals(actual));
 	}
 }
--- src/stralg13/SuffixTreeTest.java
+++ src/stralg13/SuffixTreeTest.java
@@ -11,8 +11,8 @@ public class SuffixTreeTest {
 		SuffixTree actual = new SuffixTree("a");
 		SuffixTree expected = new SuffixTree();
 		expected.string = "a" + SuffixTree.STRING_END;
-		expected.root.addEdge(0, 1);
-		expected.root.addEdge(1, 1);
+		expected.root.addEdgeAndNewNode(0, 1);
+		expected.root.addEdgeAndNewNode(1, 1);
 
 		assertTrue(actual.equals(expected));
 	}
@@ -23,9 +23,9 @@ public class SuffixTreeTest {
 
 		SuffixTree expected = new SuffixTree();
 		expected.string = "ab" + SuffixTree.STRING_END;
-		expected.root.addEdge(0, 2);
-		expected.root.addEdge(1, 2);
-		expected.root.addEdge(2, 2);
+		expected.root.addEdgeAndNewNode(0, 2);
+		expected.root.addEdgeAndNewNode(1, 2);
+		expected.root.addEdgeAndNewNode(2, 2);
 
 		assertTrue(actual.equals(expected));
 	}
@@ -36,10 +36,10 @@ public class SuffixTreeTest {
 
 		SuffixTree expected = new SuffixTree();
 		expected.string = "aba" + SuffixTree.STRING_END;
-		expected.root.addEdge(0, 1); // a
-		expected.root.edges.get(new Tuple(0,1)).addEdge(3,3); //$
-		expected.root.edges.get(new Tuple(0,1)).addEdge(1,3); //ba$
-		expected.root.addEdge(1, 3); // ba$
+		expected.root.addEdgeAndNewNode(0, 1); // a
+		expected.root.edges.get(new Tuple(0,1)).addEdgeAndNewNode(3,3); //$
+		expected.root.edges.get(new Tuple(0,1)).addEdgeAndNewNode(1,3); //ba$
+		expected.root.addEdgeAndNewNode(1, 3); // ba$
 
 		assertTrue(actual.equals(expected));
 	}
@@ -48,7 +48,7 @@ public class SuffixTreeTest {
 	public void testCompareString() {
 		SuffixTree tree = new SuffixTree("abcde");
 
-		assertTrue(tree.containsString(1, new Tuple(1, 3)));
+		assertTrue(tree.edgeStartsWithString(1, new Tuple(1, 3)));
 	}
 
 }
