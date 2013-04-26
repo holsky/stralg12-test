12141cc15efbac557bb73282e019bc4f599ca18c
holger-s <holger.s@fu-berlin.de>
2013-04-26 21:37:02 +0200

    - added split edge

--- src/stralg13/SuffixTreeTest.java
+++ src/stralg13/SuffixTreeTest.java
@@ -31,6 +31,33 @@ public class SuffixTreeTest {
 	}
 
 	@Test
+	public void testSlowScanReturnsNode() {
+		SuffixTree tree = new SuffixTree();
+		tree.string = "ab" + SuffixTree.STRING_END;
+		tree.root.addEdgeAndNewNode(0, 2);
+		
+		SlowScanResult actual = tree.slowscan(tree.root, 1);
+		assertNull(actual.edge);
+		assertTrue(actual.node.equals(tree.root));
+		
+	}
+	
+	@Test
+	public void testSlowScanReturnsEdge() {
+		SuffixTree tree = new SuffixTree();
+		tree.string = "aba" + SuffixTree.STRING_END;
+		tree.root.addEdgeAndNewNode(0, 3); // aba$
+		tree.root.addEdgeAndNewNode(1, 3); // ba$
+		
+		SlowScanResult actual = tree.slowscan(tree.root, 2);
+	
+		SlowScanResult expected 
+			= SlowScanResult.makeEdgeResult(tree.root, new Tuple(0,3), 0);
+		
+		assertTrue(actual.equals(expected));
+	}
+	
+	@Test
 	public void testSplitEdge() {
 		SuffixTree actual = new SuffixTree("aba");
 
@@ -43,12 +70,13 @@ public class SuffixTreeTest {
 
 		assertTrue(actual.equals(expected));
 	}
-		
+	
+	
 	@Test
-	public void testCompareString() {
+	public void testEdgeStartsWithString() {
 		SuffixTree tree = new SuffixTree("abcde");
 
-		assertTrue(tree.edgeStartsWithString(1, new Tuple(1, 3)));
+		assertTrue(tree.edgeStartsWithString(1, new Tuple(1, 5)));
 	}
 
 }
